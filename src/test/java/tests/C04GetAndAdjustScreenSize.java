7db55b6c557d141b3dcf92fc62721bdc5e2558b7
Fatimah Saad <fatmahsaad59@gmail.com>
2025-09-02 16:43:57 +0300

    Update TC-04: Window position and size management

--- src/test/java/tests/C04GetAndAdjustScreenSize.java
+++ src/test/java/tests/C04GetAndAdjustScreenSize.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C04GetAndAdjustScreenSize {
-    /* TC - 04
+    /* TC – 04
      * Go to the w3school URL : https://www.w3schools.com/
      * Print the position and size of the page.
      * Adjust the position and size of the page as desired.
      * Test that the page is in the position and size you want.
-     * Close the page
+     * Close the page.
      */
 
     public static void main(String[] args) throws InterruptedException {
@@ -21,18 +21,36 @@ public class C04GetAndAdjustScreenSize {
         driver.get("https://www.w3schools.com/");
 
         // Print the position and size of the page
-        System.out.println("Position: " + driver.manage().window().getPosition());
-        System.out.println("Size: " + driver.manage().window().getSize());
+        System.out.println("Initial Position: " + driver.manage().window().getPosition());
+        System.out.println("Initial Size: " + driver.manage().window().getSize());
+
+        Thread.sleep(2000);
 
         // Adjust the position and size of the page as desired
-        driver.manage().window().setPosition(new Point(100, 50));
-        driver.manage().window().setSize(new Dimension(1200, 800));
+        Point expectedPosition = new Point(100, 50);
+        Dimension expectedSize = new Dimension(1200, 800);
+
+        driver.manage().window().setPosition(expectedPosition);
+        driver.manage().window().setSize(expectedSize);
+
+        Thread.sleep(2000);
+
+        // Get the new position and size
+        Point actualPosition = driver.manage().window().getPosition();
+        Dimension actualSize = driver.manage().window().getSize();
+
+        System.out.println("New Position: " + actualPosition);
+        System.out.println("New Size: " + actualSize);
 
         // Test that the page is in the position and size you want
-        System.out.println("New Position: " + driver.manage().window().getPosition());
-        System.out.println("New Size: " + driver.manage().window().getSize());
+        if (actualPosition.equals(expectedPosition) && actualSize.equals(expectedSize)) {
+            System.out.println("Test Passed!");
+        } else {
+            System.err.println("Test Failed!");
+        }
 
         // Close the page
         driver.quit();
     }
 }
+
